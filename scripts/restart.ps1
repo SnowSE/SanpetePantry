f1d734fd4473292c4cf260c29ffb8d4f9578541c
Jonathan Allen <jallen@snow.edu>
2020-11-18 08:02:38 -0700

    Clean up restart.ps1 to remove docker-compose kill

--- scripts/restart.ps1
+++ scripts/restart.ps1
@@ -3,19 +3,11 @@ param(
     [string]$tag
 )
 
-$sha = $tag.split(":") | where  -filterscript {$_ -like  "sha*" }
+write-host "Got tag $tag from parameter"
 
-write-host "Got tag $tag from parameter, setting 'BuildID' to $sha"
+$sha = $tag.split(":") | where  -filterscript {$_ -like  "sha*" }
+write-host "Setting 'BuildID' to $sha"
 $ENV:BuildID=$sha
 
-# pull new image
-write-host "Pulling new image"
 docker-compose pull
-
-# Stop running server
-write-host "Stopping server"
-# docker-compose kill
-
-# Start server
-write-host "Starting server"
 docker-compose up -d
