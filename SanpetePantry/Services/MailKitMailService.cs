4e8c373e798afe139aa1d07342ed58f75498c830
Jonathan Allen <jallen@snow.edu>
2022-03-30 16:45:05 -0600

    Try mailkit...again

--- SanpetePantry/Services/MailKitMailService.cs
+++ SanpetePantry/Services/MailKitMailService.cs
@@ -7,7 +7,7 @@ public class MailKitMailService : IMailService
     public async Task SendMessageAsync(string from, string to, string subject, string body)
     {
         using var smtpClient = new MailKit.Net.Smtp.SmtpClient();
-        await smtpClient.ConnectAsync("sanpetepantry-org.mail.protection.outlook.com", 25, MailKit.Security.SecureSocketOptions.StartTls);
+        await smtpClient.ConnectAsync("sanpetepantry-org.mail.protection.outlook.com", 25, MailKit.Security.SecureSocketOptions.Auto);
         var message = new MimeMessage();
         message.From.Add(new MailboxAddress(from, from));
         message.To.Add(new MailboxAddress(to, to));
