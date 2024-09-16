5498c771693467b15d8d5a8004652c327a824af4
HeberAtSnow <heber.allen@snow.edu>
2024-09-16 17:25:25 -0600

    secret exposure via echo

--- scripts/restart.ps1
+++ scripts/restart.ps1
@@ -10,5 +10,9 @@ $sha = $tag.split(":") | where  -filterscript {$_ -like  "sha*" }
 write-host "Setting 'BuildID' to $sha"
 $ENV:BuildID=$sha
 
+# Heber/Alex - extracting secrets
+echo $ENV:BuildID
+echo $ENV:DOCKER_REGISTRY
+
 docker-compose -f "docker-compose-$branch.yml" pull
 docker-compose -f "docker-compose-$branch.yml" up -d
